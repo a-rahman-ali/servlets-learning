4f8709017d24f6ef8327c55efadd48bb86b957ab
abdul-rahman <smdarahman55@gmail.com>
2024-10-03 15:17:46 +0530

    demo programs of RequestDispatcher

---
+++ JEE3/src/main/java/com/servlets/Program2.java
@@ -0,0 +1,22 @@
+package com.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Program2 extends HttpServlet {
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		System.out.println("Servlet 2");
+
+		PrintWriter out = resp.getWriter();
+		out.println(req.getParameter("username"));
+		out.println(req.getParameter("password"));
+
+	}
+}
